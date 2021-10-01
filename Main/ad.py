3f3426c20e341cb1ade92324451811982a3f1b36
btheobal <baptiste.theobald@boursorama.fr>
2021-10-01 13:56:08 +0200

    Modify done, comment to do and additional functions

--- Main/ad.py
+++ Main/ad.py
@@ -1,6 +1,9 @@
+from os import close
 import bcrypt
 from user import User 
 import re
+from getpass import getpass
+import readline
 
 class AD : # Class that contains all the def that are needed in AD
     path = None
@@ -51,15 +54,16 @@ class AD : # Class that contains all the def that are needed in AD
         
             #### Get the input from the admin and check differents conditions to add a new user
             
-            first_name = input("Enter the first name of the user : ")
-            last_name = input("Enter the last name of the user : ")
-            email = input("Enter the email : ")
+            first_name = input("Enter the first name of the user : ").strip
+            last_name = input.upper("Enter the last name of the user : ").strip
+            email = input("Enter the email : ").strip
             self.check_email(email)
-            pseudo = input("Enter the pseudo of the user : ")
+            pseudo = input("Enter the pseudo of the user : ").strip
             pseudo = self.check_pseudo_loop(pseudo)
-            passwd = input("Enter the password of the user : ")
+            passwd = getpass("Enter the password of the user : \n Remember the Rules : Lenght superior to 8, one capital and small caps, a number and one special char").strip
+            passwd = self.check_password(passwd)
             passwd = self.hash_password(passwd)
-            status = input("Enter the status of the user : ")
+            status = input("Enter the status of the user : ").strip
 
             new_user = User(self.last_id, first_name, last_name, email, pseudo, passwd, status)
             new_user.user_id = self.last_id # Get the id of the last user
@@ -82,7 +86,7 @@ class AD : # Class that contains all the def that are needed in AD
             else :
                 print("please select a correct email syntax")
             check = 0
-            email = input("Enter the email again : ")    
+            email = input("Enter the email again : ").strip    
         return check
 
     def check_existing_email(self, email) : # Check if the email is already used by someone 
@@ -106,20 +110,127 @@ class AD : # Class that contains all the def that are needed in AD
             user = self.check_pseudo(pseudo)
             if user : 
                 print("This pseudo is already taken, choose an other !")  
-                pseudo = input()
+                pseudo = input().strip
             else :
                 check = True
         return pseudo 
                 
+    def generate_pseudo(self, fname, name) : # Use this if you want to generate pseudo automaticly 
+        first_l = fname[0].lower()
+        pseudo = first_l + name 
+        return pseudo
+
+    def check_password(self, password) :
+        check = 0
+        while True:  
+            if (len(password)<8):
+                check = -1
+                break
+            elif not re.search("[a-z]", password):
+                check = -1
+                break
+            elif not re.search("[A-Z]", password):
+                check = -1
+                break
+            elif not re.search("[0-9]", password):
+                check = -1
+                break
+            elif not re.search("[_@$]", password):
+                check = -1
+                break
+            elif re.search("\s", password):
+                check = -1
+                break
+            else:
+                check = 0
+                print("Valid Password")
+                break
+        if check == -1:
+            password = getpass("Not a Valid Password, select an other one").strip
+        else :
+            return password
 
 
 ############################################ MODIFIY ###################################################
-# Enter code here for modify function
+
+    def main_modify(self) : 
+        pseudo_modify = input("Please select the pseudo of the user to modify is informations \n").strip
+        user = self.check_pseudo(pseudo_modify)
+        if not user :
+            print("Please select an existing user")
+        else : 
+            check_pwd = False
+            print("If you don't want to change, just press enter \n")
+            fname = self.input_with_prefill("First name of the user : ", user.fname) # First Name 
+            name = self.input_with_prefill("Last name of the user : ", user.name) # Last Name 
+            name = name.upper()
+            email = self.input_with_prefill("Email of the user : ", user.email) # Email
+            if email != user.email : email = self.check_email(email)
+            pseudo = self.input_with_prefill("Pseudo of the user : ", user.pseudo)# Pseudo 
+            if pseudo != user.pseudo : pseudo = self.check_pseudo_loop(pseudo)
+            password = getpass("Password of the user : ")# MDP
+            if not password : password = user.password
+            else : password = self.hash_password(password)
+            status = self.input_with_prefill("Status of the user : ", user.status)# Status
+            new_user = User(user.user_id, fname, name, email, pseudo, password, status)
+
+            # Create list with all existing user 
+            list_users = self.file_to_list()
+            # Create new list with new user at the correct position
+            new_users_list = self.new_list(list_users, user.user_id, new_user)
+            # Re write in the files
+            self.print_to_file(new_users_list)
+
+# Pute the line at the exact place
+
+    def input_with_prefill(self, prompt, text, check_pwd = False):
+        def hook():
+            readline.insert_text(text)
+            readline.redisplay()
+        readline.set_pre_input_hook(hook)
+        if check_pwd : 
+            result = getpass(prompt) # MAKE THE PASSWORD INVISIBLE
+        else : result = input(prompt).strip
+        readline.set_pre_input_hook()
+        return result
+    
+    def file_to_list(self) :
+        a_file = open("users", "r")
+        list_users = []
+        for line in a_file:
+            stripped_line = line.strip()
+            line_list = stripped_line.split()
+            list_users.append(line_list)
+        a_file.close()
+        return list_users
+    
+    def new_list(self, list_users, id, new_user) :
+        i = 0 
+        id = int(id)
+        id = id - 1
+        for element in list_users :
+            if id == i : 
+                list_users[i][0] = new_user.format_for_file() 
+                list_users[i] = list(map(str.strip, list_users[i]))
+            i += 1 
+        return list_users
+
+    def print_to_file(self, new_user_list) : 
+        i = 0
+        f = open('users', 'r+')
+        f.truncate(0)
+        a_file = open("users", "w")
+        for element in new_user_list:
+            elem_to_print = str(element[0])
+            a_file.write(elem_to_print + "\n")
+            i += 1 
+        a_file.close()
+    
 
 ############################################ DELETE ####################################################
     
     def main_delete(self, pseudo) :
-        pseudo_delete = input("Enter the pseudo to delete all the informations about the user \n")
+        pseudo_delete = input("Enter the pseudo to delete all the informations about the user \n").strip
         line_id = self.find_line_user(pseudo_delete)
         if line_id != False : 
             if pseudo == pseudo_delete :
@@ -129,8 +240,8 @@ class AD : # Class that contains all the def that are needed in AD
                 self.delete_pseudo_line(line_id)
         else :
             print("Select an existing pseudo !")
+    
     def delete_pseudo_line(self, line_id) :
-        print(line_id)
         a_file = open("users", "r")
         lines = a_file.readlines()
         a_file.close()
@@ -163,7 +274,7 @@ class AD : # Class that contains all the def that are needed in AD
         user = password_check = False 
 
         while not user : 
-            pseudo = input("Enter your pseudo \n") 
+            pseudo = input("Enter your pseudo \n").strip 
             user = self.check_pseudo(pseudo) # Check if the pseudo exist
             if not user : 
                 print("Select a existing user ! \n")          
@@ -171,7 +282,7 @@ class AD : # Class that contains all the def that are needed in AD
         count = 3 
         while True :  
             print()
-            password = input("Now enter the password \n")
+            password = getpass("Now enter the password \n")
             id = self.find_id_user(pseudo)
             passwd = self.find_existing_passwd(id)
             password_check = self.check_passwd(password, passwd) 
@@ -197,17 +308,15 @@ class AD : # Class that contains all the def that are needed in AD
         return False
     
     def find_id_user(self, pseudo) :
-        with open('users') as f:
-            lines = f.read().splitlines()
-        i = 0
-        for element in lines :
-            if pseudo in element :
-                break
-            i += 1 
-        return i
+        for user in self.userlist :
+            if user.pseudo == pseudo :
+                return user.user_id
+        return False
 
     def find_existing_passwd(self, id) :
         a_file = open("users")
+        id = int(id)
+        id = id - 1
         line_to_read = [id]
         for position, line in enumerate(a_file):
             if position in line_to_read:
@@ -227,7 +336,7 @@ class AD : # Class that contains all the def that are needed in AD
         
         while True : # While loop to keep the user in the program 
             self.show_menu_user() # Print the menu  
-            command = input() # Input from user to select an action
+            command = input().strip # Input from user to select an action
             if command == "1" : 
                 self.display_current_user()  
             elif command == "2" :
@@ -246,7 +355,7 @@ class AD : # Class that contains all the def that are needed in AD
         
         while True : # While loop to keep the user in the program 
             self.show_menu_admin() # Print the menu
-            command = input() # Input from user to select an action
+            command = input().strip # Input from user to select an action
             if command == "1" : 
                 self.display_main_display()  
             elif command == "2" :
@@ -254,7 +363,7 @@ class AD : # Class that contains all the def that are needed in AD
             elif command == "3" :
                 self.main_delete(pseudo)  # TODO
             elif command == "4" :
-                modify.main_modify()  # TODO
+                self.main_modify()  # TODO
             elif command == "0" :
                 print("Good bye :)")
                 break
@@ -279,7 +388,7 @@ class AD : # Class that contains all the def that are needed in AD
 
     def display_main_display(self) : # Main display menu
         self.print_menu_users() 
-        d_command = input() 
+        d_command = input().strip
 
         while d_command != "3" : 
             if d_command == "1" : 
@@ -288,7 +397,7 @@ class AD : # Class that contains all the def that are needed in AD
                 self.display_login()
 
             self.print_menu_users()
-            d_command = input()
+            d_command = input().strip
             
     def print_menu_users(self) : # Display menu
         print("\n")
